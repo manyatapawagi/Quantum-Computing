9fcb649a0a73e9f7f176bd2033963439bd304790
Manyata Pawagi <manyatapawagi@gmail.com>
2021-08-28 23:43:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var loader = document.getElementById("loader");
+    var blacker = document.getElementById("container");
+    var body = document.getElementsByTagName("body");
+    function onloadFunction(){
+        loader.style.animation = "none";
+        blacker.style.display = "none";
+    };
